9a266c432c770e0c12e6a4630371a3b85ebd0d51
maxsg5 <maxsg5@hotmail.com>
2021-10-01 16:59:19 -0600

    rework of the character prefab

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -12,84 +12,71 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
 
-    //private Weapon weapon; // The weapon the character is holding
-    //private Health health; // Reference to the health script.
+    
     public float speed = 10f; // The speed the character moves at.
     public float jumpForce = 5f; // The force applied to the character when it jumps.
+    public Transform groundCheck; // The position of the ground check.
+    public float checkRadius; // The radius of the ground check.
+    public LayerMask whatIsGround; // The layer that is considered ground.
+    public int extraJumpsValue; // The amount of extra jumps the character has.
+
+
     private Rigidbody2D rb2d; // Reference to the players rigidbody.
+    private int extraJumps; // The number of extra jumps the character has.
     private bool isGrounded = false; // Whether or not the character is grounded.
     private bool canDoubleJump = false; // Whether or not the character can double jump.
+    //private Weapon weapon; // The weapon the character is holding
+    //private Health health; // Reference to the health script.
+
 
     // Start is called before the first frame update
     void Start()
     {
+        extraJumps = extraJumpsValue;
         //health = GetComponent<Health>(); // Get the health script
         //weapon = GetComponentInChildren<Weapon>(); // Get the weapon script from the child object
         rb2d = GetComponent<Rigidbody2D>(); // Get the rigidbody
     }
 
+
+    // FixedUpdate should be used when applying forces, torques, or other physics-related functions 
+    void FixedUpdate()
+    {
+        // Check if the character is grounded.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround); 
+
+        //movement
+        float x = Input.GetAxis("Horizontal"); // Get the horizontal input
+        Vector2 velocity = new Vector2(x, 0); // Create a new vector2 with the x value of the horizontal input
+        rb2d.velocity = new Vector2(x * speed, rb2d.velocity.y);  // Set the velocity of the rigidbody to the velocity created above
+
+    }
     // Update is called once per frame
     void Update()
     {
-
-        //movement
-        HandleMovement();
-        Debug.Log(isGrounded);
-
-
+        //Jumping
+        if(isGrounded)
+        { 
+            extraJumps = extraJumpsValue; // Reset the extra jumps
+        }
+         //jumping
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0) // If the space key is pressed and the character has extra jumps
+        {
+            rb2d.velocity = Vector2.up * jumpForce; // Add a force to the rigidbody in the up direction
+            extraJumps--; // Decrease the number of extra jumps
+        } else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded) // If the space key is pressed and the character has no extra jumps and is grounded
+        {
+            rb2d.velocity = Vector2.up * jumpForce; // Add a force to the rigidbody in the up direction
+        }
         
     }
 
-    /// <summary>
-    /// Handles the movement of the character.
-    /// </summary>
-    /// Author: Max Schafer
-    private void HandleMovement(){
-        float x = Input.GetAxis("Horizontal"); // Get the horizontal input
-        Vector2 velocity = new Vector2(x, 0); // Create a new vector2 with the x value of the horizontal input
-        rb2d.velocity = velocity * speed; // Set the velocity of the rigidbody to the velocity created above
+   
 
-        //jumping
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (isGrounded)
-            {
-                
-                Jump();
-                canDoubleJump = true;
-
-            }
-            else if (canDoubleJump)
-            {
-                Jump();
-                canDoubleJump = false;
-            }
-        }
-
-    }
-
-    /// <summary>
-    /// Handles the jumping of the character.
-    /// </summary>
-    /// Author: Max Schafer
-    private void Jump(){
-        rb2d.velocity = Vector2.up * jumpForce; // Add a force to the rigidbody in the up direction
-        
-    }
+    
 
 
-    /// <summary>
-    /// Checks for collisions.
-    /// </summary>
-    /// Author: Max Schafer
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        //check if the player is on the ground
-        if (col.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
+   
 
 
 }
