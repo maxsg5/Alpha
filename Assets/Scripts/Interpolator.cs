168ddaa51e3a0a14604dd05332aa14a345144b94
Josh Coss <josh.coss@gmail.com>
2021-10-14 20:43:35 -0600

    Updated Enemy and Sensor files for 2D
    Updated Enemy.cs, GroundTrooper.cs, and Bomber.cs in order to account for 2D development.
    
    Updated SphereSensor.cs and SectorSensor.cs in order to account for 2D development
    
    Rewrote PathMove.cs to better take advantage of 2D development and gravity

--- Assets/Scripts/Interpolator.cs
+++ Assets/Scripts/Interpolator.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Stores an array of control (Vector3) points and calculates the interpolated
+/// Stores an array of control (Vector2) points and calculates the interpolated
 /// position
 /// </summary>
 /// 
 /// Author: Josh Coss   (JC)
 /// 
 /// Variables
-/// control         List of Vector3 control points
+/// control         List of Vector2 control points
 /// closed          True if path is closed, false if not
 /// length          Number of points in the control path
 /// lengthM1        Length minus 1
 public abstract class Interpolator
 {
-    public Vector3[] control;
+    public Vector2[] control;
     public bool closed;
     public int length;
 
     protected int lengthM1;
 
     /// <summary>
-    /// Sets the control points and closed value based on a list of Vector3 points
+    /// Sets the control points and closed value based on a list of Vector2 points
     /// </summary>
-    /// <param name="points">List of Vector3 points</param>
+    /// <param name="points">List of Vector2 points</param>
     /// <param name="closed">Closed value</param>
     /// 
     /// Date        Author      Description
     /// 2021-10-13  JC          Initial Testing
-    public Interpolator (Vector3[] points, bool closed) {
+    public Interpolator (Vector2[] points, bool closed) {
         SetControl(points);
         SetClosed(closed);
     }
@@ -49,19 +49,19 @@ public abstract class Interpolator
     }
 
     /// <summary>
-    /// Takes in a list of vector3 points and sets them as the list of control points
+    /// Takes in a list of Vector2 points and sets them as the list of control points
     /// </summary>
-    /// <param name="points">List of Vector3 points</param>
+    /// <param name="points">List of Vector2 points</param>
     /// 
     /// Date        Author      Description
     /// 2021-10-13  JC          Initial Testing
-    public void SetControl (Vector3[] points) {
+    public void SetControl (Vector2[] points) {
         control = points;
     }
 
     /// <summary>
     /// Takes in a list of Transform points, calculates lengthM1, and puts each transform
-    /// point into a list of vector3 objects. Then sets as list of control points.
+    /// point into a list of Vector2 objects. Then sets as list of control points.
     /// </summary>
     /// <param name="points">List of Transform points</param>
     /// 
@@ -71,7 +71,7 @@ public abstract class Interpolator
         length = points.Length;
         lengthM1 = length - 1;
 
-        control = new Vector3[length];
+        control = new Vector2[length];
         for (int i=0;i < length; i++)
             control[i] = points[i].position;
     }
@@ -87,6 +87,6 @@ public abstract class Interpolator
         closed = newClosed;
     }
 
-    public abstract Vector3 Evaluate (float u);
-    public abstract Vector3 Heading (float u);
+    public abstract Vector2 Evaluate (float u);
+    public abstract Vector2 Heading (float u);
 }
--- Assets/SphereSensor.cs
+++ Assets/SphereSensor.cs
@@ -2,31 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Check to see if an object can see a target in a circle around it
+/// </summary>
+/// 
+/// Author: Josh Coss 	(JC)
+/// 
+/// Variables
+/// radius			Radius of circle
+/// radius2			Radius squared
 public class SphereSensor : Sensor
 {
 	public float radius;
 	public float radius2;
 
+	/// <summary>
+	/// Sets the radius of the circle
+	/// </summary>
+	/// 
+	/// Date		Author		Description
+	/// 2021-10-14	JC			Initial testing
 	void Start()
 	{
 		SetRadius(radius);
 	}
 
+	/// <summary>
+	/// Sets the radius of the circle and calculates radius2
+	/// </summary>
+	/// <param name="r">radius, float</param>
+	/// 
+	/// Date		Author		Description
+	/// 2021-10-14	JC			Initial testing
 	public void SetRadius(float r)
 	{
 		radius = r;
 		radius2 = r * r;
 	}
 
+	/// <summary>
+	/// Check to see if the sensor can see the target, returns true if so
+	/// </summary>
+	/// <param name="target">Transform of target</param>
+	/// <returns>bool</returns>
+	/// 
+	/// Date		Author		Description
+	/// 2021-10-14	JC			Initial testing
+	/// 2021-10-14	JC			Updated for 2D
 	public override bool CanSee(Transform target)
 	{
 		Vector3 delta = target.position - transform.position;
 
 		if (delta.sqrMagnitude <= radius2)
 		{
-			RaycastHit hit;
-			if (Physics.Raycast(transform.position, delta, out hit, radius))
-				return hit.transform == target;
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, delta, radius);
+			if (hit.transform == target)
+			{
+				return true;
+			}
 		}
 		return false;
 	}
