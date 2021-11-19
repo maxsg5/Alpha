b55eb7dd48944992f144427936e10066e56aed6d
maxsg5 <maxsg5@hotmail.com>
2021-11-19 16:25:24 -0700

    added sound effects to switches and the door

--- Assets/ChangeToGreen.cs
+++ Assets/ChangeToGreen.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This is used to change the signal lights to green once the player has interacted with a coresponding switch.
+/// </summary>
+/// Author: Max Schafer
+/// Date: 2021-11-19
+/// Description: Initial testing
 public class ChangeToGreen : MonoBehaviour
 {
-    private Light light;
-    // Start is called before the first frame update
+    private Light light; // The light that will be turned green.
+    
+    /// <summary>
+    /// Initialize the Light component.
+    /// </summary>
+    /// Author: Max Schafer
+    /// Date: 2021-11-19
+    /// Description: Initial testing
     void Start()
     {
         light = GetComponent<Light>();
     }
 
+    /// <summary>
+    /// Changes light colour to green.
+    /// </summary>
+    /// Author: Max Schafer
+    /// Date: 2021-11-19
+    /// Description: Initial testing
     public void ChangeColor()
     {
         light.color = Color.green;
